bdbaf8cf0ba0419e7e40d4a10adb577f88396eb6
RuthrapathyK <kruthrapathy@gmail.com>
2025-08-10 16:58:31 +0530

    Review Addressed

--- inc/tasks.h
+++ inc/tasks.h
@@ -2,7 +2,6 @@
 #define __TASK_H
 
 #include "common.h"
-#include "common.h"
 
 #define MAX_TASK_LIMIT 10
 
@@ -24,6 +23,15 @@ typedef struct {
 	task_state_e TaskState; // Current State of the Task
 } Task_type;
 
+/**
+ * @brief Initialize  and Schedule the New Task
+ * 
+ * @param stackAddr Starting address of the Stack pointer 
+ * @param stackSize_words Total Size of Stack in Words(i.e 32 bits)
+ * 						  Minimum 16 word size is required for scheduler opeartion itself. So choose >16
+ * @param taskPtr Address of the Task Function
+ * @param prio Priority of the Task. 1 is the highest priority and 255 is the lowest priority
+ */
 void createTask(uint32_t *stackAddr, uint32_t stackSize_words, void (*taskPtr)(), uint8_t prio);
 
 #endif
