51e6d444c4b60a894f637eb707013e526e37a200
RuthrapathyK <kruthrapathy@gmail.com>
2025-12-20 01:52:49 +0530

    Removed unwanted changes

--- src/TouchDisplay/display.c
+++ src/TouchDisplay/display.c
@@ -42,105 +42,6 @@ static void Disp_dataCommand_Select(Disp_cmdData_e flag)
 static void Disp_delayMS(uint32_t ms)
 {
     OS_delay(ms);
-} 
-
-void Disp_HardReset(void)
-{
-      /* Pull the Reset pin to Low */
-      GPIO_clearPin(PE1);
-
-      /* Wait till minimum time is elapsed */
-      Disp_delayMS(20);
-
-      /* Deassert the Reset Pin to Switch the Display ON */
-      GPIO_setPin(PE1);
-
-      /* Wait for the Display to complete the Startup */
-      Disp_delayMS(200);
-}
-
-void Disp_readReg(uint8_t cmd, uint16_t *rx_buf, uint32_t rx_bitLen)
-{
-   uint16_t t_txBuf = cmd;
-   uint32_t rx_ByteLen = 0;
-   uint32_t rx_frameBitLen = 0;
-
-   /* Enable Chip Select */
-   Disp_chipSelect(Disp_pinLow);
-
-   /* Set the Display to Command Mode*/
-   Disp_dataCommand_Select(Disp_enableCommand);
-
-   /* Send the Command */
-   SPI_Send(&t_txBuf, 1);
-
-   /* Set the Display to Data Mode*/
-   Disp_dataCommand_Select(Disp_enableData);
-   
-   /* Derive the SPI0 modules Frame Bit Length */
-   if(rx_bitLen > 8)
-      rx_frameBitLen = rx_bitLen + 1;
-   else
-      rx_frameBitLen = rx_bitLen;
-
-   /* Calculate the Number of Bytes from Bit Length */
-   rx_ByteLen  = rx_frameBitLen / 8;
-
-   if((rx_frameBitLen % 8) != 0)
-      rx_ByteLen = rx_ByteLen + 1;
-      
-   /* Get the Data from the Dispaly Unit */
-   SPI_Receive(rx_buf, rx_ByteLen);
-
-   /* Rearrage the received Bytes according if Bit Length >8bits*/
-   if(rx_bitLen > 8)
-   {
-      for(uint32_t iter = 0; iter < rx_ByteLen - 1; iter++)
-      {
-         rx_buf[iter] = ((((rx_buf[iter] << 1) & 0xFE) | ((rx_buf[iter + 1] >> 7) & 0x01)) & 0xFF);
-      }
-   }
-
-
-   /* Disable Chip Select */
-   Disp_chipSelect(Disp_pinHigh);
-}
-
-void Disp_writeReg(uint8_t cmd, uint16_t *paramData_buf, uint32_t paramData_bitLen)
-{
-      uint16_t t_txBuf = cmd;
-      uint32_t tx_ByteLen = 0;
-
-      /* Enable Chip Select */
-      Disp_chipSelect(Disp_pinLow);
-
-      /* Set the Display to Command Mode*/
-      Disp_dataCommand_Select(Disp_enableCommand);
-
-      /* Send the Command */
-      SPI_Send(&t_txBuf, 1);
-
-      /* Check if teh Data or Command Parameter needs to be written */
-      if(paramData_bitLen > 0)
-      {
-            /* Set the Display to Data Mode*/
-            Disp_dataCommand_Select(Disp_enableData);
-
-            /* Calculate the Number of Bytes from Bit Length */
-            tx_ByteLen  = paramData_bitLen / 8;
-
-            if((paramData_bitLen % 8) != 0)
-                  tx_ByteLen = tx_ByteLen + 1;
-
-            /* Send the Parameter of the Command / Data to be written in Memory */
-            SPI_Send(paramData_buf, tx_ByteLen);
-      }
-
-      /* Set the Display to Data Mode*/
-      Disp_dataCommand_Select(Disp_enableData);
-
-      /* Disable Chip Select */
-      Disp_chipSelect(Disp_pinHigh);
 }
 
 void Disp_setBrightness(uint8_t bright_val)
@@ -148,7 +49,7 @@ void Disp_setBrightness(uint8_t bright_val)
       uint16_t t_val =  bright_val;
 
       /* Set the Brightness Value */
-      Disp_writeReg(DISP_CMD_WR_DISPLAY_BRIGHTNESS, &t_val, 8);  
+      ILI_writeReg(ILI_CMD_WR_DISPLAY_BRIGHTNESS, &t_val, 8);  
       
       Disp_delayMS(25);
 }
@@ -158,7 +59,7 @@ uint16_t Disp_getScanLine(void)
    uint16_t ret_val = 0;
    uint16_t t_data[2] = {0};
 
-   Disp_readReg(DISP_CMD_GET_SCANLINE, t_data, 16);
+   ILI_readReg(ILI_CMD_GET_SCANLINE, t_data, 16);
 
    ret_val = (((t_data[0] << 8) | t_data[1]) & 0x3FF);
 
@@ -170,35 +71,18 @@ void Disp_Init(void)
       uint16_t selfDiag_1 = 0;
       uint16_t selfDiag_2 = 0;
 
-      /* Initialize SPI0 peripheral with configurations suitable for ILI9341 */
-      SPI_Init();
-
-      /* Configure Pins for SPI0 */
-      Pin_Config(Port_PA, 2, PA2_SSI0CLK);
-      Pin_Config(Port_PA, 4, PA4_SSI0RX);
-      Pin_Config(Port_PA, 5, PA5_SSI0TX);
-      
-      /* Initialize Chip Select Pin - Initially High */
-      GPIO_Init(PA3, GPIO_DigitalOutput, 1);
-
-      /* Initialize the Reset pin  - Initially High */
-      GPIO_Init(PE1, GPIO_DigitalOutput, 1);
-      
-      /* Initialize the DC/RS pin (Data/Command)  - Initially DC */
-      GPIO_Init(PE2, GPIO_DigitalOutput, 1);
-
-      /* Perform Hardware Reset of Display */
-      Disp_HardReset();
+      /* Initialize the Display Driver */
+      ILI_DriverInit();
 
       /* Read Diagnostic bits */
-      Disp_readReg(DISP_CMD_READ_SELFDIAG_RESULT, &selfDiag_1, 8);
+      ILI_readReg(ILI_CMD_READ_SELFDIAG_RESULT, &selfDiag_1, 8);
 
       /* Set the Display Module to Sleep Out */
-      Disp_writeReg(DISP_CMD_SLPOUT, paramBuff, 0);
+      ILI_writeReg(ILI_CMD_SLPOUT, paramBuff, 0);
       Disp_delayMS(200);
 
       /* Check whether the Read Diagnostic bits are inverted */
-      Disp_readReg(DISP_CMD_READ_SELFDIAG_RESULT, &selfDiag_2, 8);
+      ILI_readReg(ILI_CMD_READ_SELFDIAG_RESULT, &selfDiag_2, 8);
       if(((selfDiag_1 ^ selfDiag_2) >> 6) != 3)
             ASSERT(0);
 
@@ -230,7 +114,7 @@ void Disp_MemoryWrite_18bit(void)
       pixel_data.Pixel_Green = rgb_green << 2;      
 
       /* Send only command to write Pixel Data */
-      Disp_writeReg(DISP_CMD_RAMWR, (uint16_t *)&pixel_data, 0);
+      ILI_writeReg(ILI_CMD_RAMWR, (uint16_t *)&pixel_data, 0);
 
       /* Enable Chip Select */
       Disp_chipSelect(Disp_pinLow);
@@ -259,28 +143,35 @@ void Disp_setPixel_Pointer(uint16_t cur_col_addr, uint16_t cur_pg_addr, uint16_t
       t_buf[1] = (cur_col_addr & 0xFF);          // SC7:SC0
       t_buf[2] = ((t_max_col_addr >> 8) & 0x01); // EC15:EC8
       t_buf[3] = (t_max_col_addr & 0xFF);        // EC7:EC0
-      Disp_writeReg(DISP_CMD_CASET, t_buf, 32);
+      ILI_writeReg(ILI_CMD_CASET, t_buf, 32);
 
       /* Set the Page Address */
       t_buf[0] = ((cur_pg_addr >> 8) & 0x01); // SP15:SP8
       t_buf[1] = (cur_pg_addr & 0xFF); // SP7:SP0
       t_buf[2] = ((t_max_pg_addr >> 8) & 0x01); // EP15:EP8
       t_buf[3] = (t_max_pg_addr & 0xFF); // EP7:EP0
-      Disp_writeReg(DISP_CMD_RASET, t_buf, 32);
+      ILI_writeReg(ILI_CMD_RASET, t_buf, 32);
 
 }
+// void Disp_sendPixels(Disp_Pixel_type *Pixel, uint32_t col_start, uint32_t pg_start, uint32_t length)
+// {
+//       ILI_Pixel_type t_pxl_dat = {0};
 
-void Disp_MemoryRead_18bit()
-{
-      Disp_setPixel_Pointer(0, 0, 240, 320);
+//       /* Set the Pixel Pointer */
+//       Disp_setPixel_Pointer(col_start, pg_start, 240, 320);
 
-      /* Read the Pixel Data */
-      //Disp_readReg(DISP_CMD_RAMRD, rx_buffer, 5760);
-}
+//       /* Map the Display data to ILI9341 driver IC */
+//       t_pxl_dat.ILI_PixRed = Pixel->Dis_PixRed;
+//       t_pxl_dat.ILI_PixBlue = Pixel->Dis_PixBlue;
+//       t_pxl_dat.ILI_PixGreen = Pixel->Dis_PixGreen;
 
+//       /* Send the Pixel data to Driver IC */
+//       ILI_MemWrite_18bit(&t_pxl_dat, length);
+// }
 void Disp_Run(void)
 {
      count %= 4;
+
      switch(count)
      {
       case 0:
